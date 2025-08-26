a03b1879cff32aa2cf3b587f546b63b3f1320f12
alberto <albertoborsatto@hotmail.com>
2025-08-25 22:00:51 -0300

    class id fixed

--- back/Models/Class.cs
+++ back/Models/Class.cs
@@ -1,5 +1,6 @@
 public class Class {
     [Key]
+    public int Id { get; set; }
     public int Number { get; set; }
     public int SeatsNum { get; set; }
     public bool HasProjector { get; set; }
