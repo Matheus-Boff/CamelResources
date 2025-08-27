19fb16652cb121d497148e62db819c18dfaadba7
Matheus-Boff <153229200+Matheus-Boff@users.noreply.github.com>
2025-08-27 17:33:56 -0300

    Delete front/CamelResourcesFront/src/app/app.routes.ts

--- front/CamelResourcesFront/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './login/login';
-import { HomePage } from './home-page/home-page';
-import { ResourcesPageComponent } from './resources-page/resources-page.component';
-
-export const routes: Routes = [
-    { path: '', component: Login },
-    { path: 'home', component: ResourcesPageComponent }
-];
