209a6bd2c546bc945c9450727093f4e0e56360d7
alberto <albertoborsatto@hotmail.com>
2025-08-26 21:39:35 -0300

    laboratorio data flow implemented

---
+++ back/Services/Interfaces/ILaboratorioService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using back.Models;
+
+namespace back.Services.Interfaces
+{
+    public interface ILaboratorioService
+    {
+        Task<IEnumerable<Laboratorio>> GetAllAsync();
+        Task<Laboratorio> GetByIdAsync(int id);
+        Task UpdateAsync(int id, Laboratorio lab);
+    }
+}
