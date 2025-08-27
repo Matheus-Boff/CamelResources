4ae294f4b777967ad45468658427e0cf05a1532e
mecontudo <mariaescontu@gmail.com>
2025-08-27 13:46:23 -0300

    header adjusted + calendar at homepage

--- front/CamelResourcesFront/src/app/home-page/home-page.ts
+++ front/CamelResourcesFront/src/app/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CalendarComponent } from '../calendar/calendar';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
