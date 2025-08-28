b0c24e15e83dc064eff6bcf8f4b1ad6d00c6e58d
Mariana Adam dos Anjos <mariana.danjos@hotmail.com>
2025-08-28 13:16:50 -0300

    modal pronto com cores para ajustar

--- front/CamelResourcesFront/src/app/app.routes.ts
+++ front/CamelResourcesFront/src/app/app.routes.ts
@@ -5,9 +5,10 @@ import { LaboratoriosPage } from './laboratorios-page/laboratorios-page';
 import { SalasPage } from './salas-page/salas-page';
 import { RelatorioPage } from './relatorio-page/relatorio-page';
 import { ResourcesPageComponent } from './resources-page/resources-page.component';
+import { ModalViewComponent } from './modal-view/modal-view.component';
 
 export const routes: Routes = [
-    { path: '', component: Login },
+    { path: '', component: ModalViewComponent },
     { path: 'home', component: HomePage },
     { path: 'laboratorios', component: LaboratoriosPage },
     { path: 'salas', component: SalasPage },
